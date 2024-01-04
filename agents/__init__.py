ba4183df92f622dc672ef388e4f38a1e465ea91d
lerner101 <95956228+lerner101@users.noreply.github.com>
2024-01-03 23:23:21 -0500

    Update __init__.py

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,13 +2,6 @@ from .agent import Agent
 from .random_agent import RandomAgent
 from .human_agent import HumanAgent
 from .student_agent import StudentAgent
-from .oppy_agent import OppyAgent
-from .oppy_agent2 import Oppy2Agent
-from .student2_agent import Student2Agent
-from .oppy_agent3 import Oppy3Agent
-from .student3_agent import Student3Agent
-from .oppy_agent4 import OppyAgent4
-from .BenAndRudi_agent import BenAndRudiAgent
-from .novick_agent import NovickAgent
+
 
 
